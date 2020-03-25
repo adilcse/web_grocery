0bc43ae34be2c827fdf06d9905e049d0a08d37ef
adilcse <adil.cs.work@gmail.com>
2020-03-25 17:30:14 +0530

    catagory fixed

--- src/components/catagory/CatagoryCard.js
+++ src/components/catagory/CatagoryCard.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const CatagoryCard=(props)=>{
 return(
     <Card style={{ width: '18rem',display:'inline-block' }}>
-        <Link to={`/catagories/${props.item.id}`}>
+        <Link to={`/catagories/${props.item.catId}`}>
             <Card.Img variant="top" src={props.item.image} />
         </Link>   
    
